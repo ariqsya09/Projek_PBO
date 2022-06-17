0d1f2499704f0958cffd8f1241bc5199e137606d
Svdex <ucihaibna@yahoo.com>
2022-06-17 08:20:59 +0700

    Add item class

---
+++ Projek_PBO/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projek_PBO
+{
+    internal class Item
+    {
+        public int Id { get; }
+        public string Nama { get; }
+        public int Stock { get; }
+        public int Harga { get; }
+
+        public Item(int id, string nama, int harga, int stock = 1)
+        {
+            Id = id;
+            Nama = nama;
+            Stock = stock;
+            Harga = harga;
+        }
+
+    }
+}
