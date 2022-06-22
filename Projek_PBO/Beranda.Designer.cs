43a7af031ff5aeed8ed69f814b7d35703e80009f
Svdex <ucihaibna@yahoo.com>
2022-06-22 14:03:28 +0700

    Update beranda

--- Projek_PBO/Beranda.Designer.cs
+++ Projek_PBO/Beranda.Designer.cs
@@ -31,13 +31,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.logout = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(268, 99);
+            this.button1.Location = new System.Drawing.Point(201, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 55);
+            this.button1.Size = new System.Drawing.Size(154, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Manajamen Barang";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,9 +47,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(268, 187);
+            this.button2.Location = new System.Drawing.Point(201, 130);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(205, 55);
+            this.button2.Size = new System.Drawing.Size(154, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "Transaksi";
             this.button2.UseVisualStyleBackColor = true;
@@ -55,23 +58,35 @@
             // 
             // logout
             // 
-            this.logout.Location = new System.Drawing.Point(268, 277);
+            this.logout.Location = new System.Drawing.Point(201, 264);
+            this.logout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.logout.Name = "logout";
-            this.logout.Size = new System.Drawing.Size(205, 50);
+            this.logout.Size = new System.Drawing.Size(154, 41);
             this.logout.TabIndex = 2;
             this.logout.Text = "Logout";
             this.logout.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(201, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "History Transaksi";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Beranda
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.logout);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Beranda";
-            this.Text = "Form1";
+            this.Text = "Beranda";
             this.ResumeLayout(false);
 
         }
@@ -81,6 +96,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Button button3;
     }
 }
 
